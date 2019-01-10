3b73cf41ecca0144a9c84ef038fbbf2c92bcc162
Kapil Reddy <kapil@helpshift.com>
2019-01-10 19:26:44 +0530

    Remove `implicits` from project.clj
    cider-nrepl plugin does not work with `implicits` set to false

--- project.clj
+++ project.clj
@@ -7,8 +7,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.8.1"
-  :implicits false
-
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/core.async "0.4.490"]
                  [org.clojure/test.check "0.9.0"]
