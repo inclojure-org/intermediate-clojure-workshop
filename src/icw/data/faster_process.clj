313dc1145e8fa207912772be2a66dfe011144669
Kapil Reddy <kapil@helpshift.com>
2019-01-10 13:20:48 +0530

    Add concurrency chapter

--- src/icw/data/faster_process.clj
+++ src/icw/data/faster_process.clj
@@ -3,9 +3,10 @@
             [icw.data.process :as idp]))
 
 
-;; Deref and how it's useful
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Future
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ;; This will run on a different thread
 (future 1)
@@ -18,7 +19,9 @@
 
 ;; What's the point of deref then? Is it to just make threds block?
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Promise
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Deliver works only once on a promise
 ;; Deref works on promise as well
@@ -33,7 +36,10 @@
 
 ;; Promises are good to collect the first result from multiple threads/sources
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Delay
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; A very subtle difference than future. The body runs only after it's
 ;; deref and it only runs ons
@@ -44,6 +50,45 @@
   @a
   @a)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Atoms
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(def state (atom {}))
+
+;; You can change the information of an atom using swap! or reset!
+
+(swap! state assoc :a 1)
+
+;; Derefing the state will give you the current value
+@state
+
+
+;; @TODO add exercises for using future
+;; @TODO add exercises that use delay
+;; @TODO add exercises that use promise
+
+;; @TODO add exercise that uses agent
+
+;; @TODO add exercises that use future, delay and promise
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Rock scissor paper
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def BEATS {:rock :scissors, :paper :rock, :scissors :paper})
+
+(defn winner
+  [[name-1 move-1] [name-2 move-2]]
+  (cond
+   (= move-1 move-2) "no one"
+   (= move-2 (BEATS move-1)) name-1
+   :else name-2))
+
+(defn judge
+  [p1 p2]
+  (future [@p1 @p2 (winner @p1 @p2)]))
 
 (defn populate-db
   []
@@ -54,3 +99,8 @@
           albums (idp/source->album-xs idp/album-source)]
       (doseq [album albums]
         (jdbc/insert! album)))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Caveats
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
